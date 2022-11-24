bf66a00f002115ed9070aa227f50a02df5536ac5
Alexandre Bouriez <alexandre.bouriez@laposte.net>
2022-11-24 14:10:18 +0100

    Ajout de ma réponse à l'exercice 5

--- Exercice.ts
+++ Exercice.ts
@@ -84,3 +84,21 @@ function deternmineHighestNumber(
 }
 
 console.log(deternmineHighestNumber(2, 25, 15));
+
+// Exercice 5
+/**
+ * Détermine le plus grand nombre dans un ensemble
+ * @param numberList ensemble des nombres
+ * @returns une phrase indiquant le plus grand nombre
+ */
+function highestAmongMany(...numberList:number[]):string{
+    let highestNumber = 0;
+    numberList.forEach(number => {
+        if (number > highestNumber){
+            highestNumber = number
+        }
+    });
+    return `Le plus grand nombre est ${highestNumber}`
+}
+
+console.log(highestAmongMany(2,0,12,108,840,65, 42))
