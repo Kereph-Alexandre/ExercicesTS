84393348382deaf90d5f550d364c5713877c553b
Alexandre Bouriez <alexandre.bouriez@laposte.net>
2022-11-24 14:36:05 +0100

    Ajout de ma réponse à l'exercice 6

--- Exercice.ts
+++ Exercice.ts
@@ -102,3 +102,57 @@ function highestAmongMany(...numberList:number[]):string{
 }
 
 console.log(highestAmongMany(2,0,12,108,840,65, 42))
+
+// Exercice 6
+
+/**
+ * Affiche dans la console le nom d'un élève, sa moyenne, et le commentaire associé
+ * @param studentName 
+ * @param studentNotes l'ensemble de ses notes
+ */
+function evaluateStudent(studentName:string, ...studentNotes:number[]):void{
+    const studentAverage:number = calculateAverage(...studentNotes);
+    const studentMention: string = addMention(studentAverage);
+
+    console.log(`${studentName} a une moyenne de ${studentAverage} : c'est ${studentMention}`)
+}
+
+/**
+ * Calcule la moyenne d'un élève à partir de l'ensemble de ses notes
+ * @param numbers les notes de l'éleve
+ * @returns la moyenne de l'élève
+ */
+function calculateAverage(...numbers:number[]):number {
+    let notesAverage:number = 0;
+
+    numbers.forEach(number => {
+        notesAverage += number        
+    });
+
+    notesAverage /= numbers.length;
+
+    return notesAverage
+}
+
+/**
+ * Etablit la mention accompagnant le dossier d'un élève
+ * @param note la moyenne de l'élève
+ * @returns la mention
+ */
+function addMention(note:number):string{
+    let mention:string = '';
+
+    if (note>20 || note<0){
+        mention = `note invalide`;
+    } else if (note <5){
+        mention = `Catastrophique`;
+    } else if (note <11){
+        mention = `Insuffisant`;
+    } else if (note < 15){
+        mention = `Passable`;
+    } else if (note < 19){
+        mention = `Bien`;
+    } else mention = `Très Bien`
+
+    return mention
+}
