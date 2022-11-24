e41b08f1c6b10eac0c36b84a5db7134c4e83e1f0
Alexandre Bouriez <alexandre.bouriez@laposte.net>
2022-11-24 11:40:02 +0100

    Ajout de ma réponse à l'exercice 2

--- Exercice.ts
+++ Exercice.ts
@@ -17,4 +17,19 @@ accueil("Alexandre");
 
 // Exercice 2
 
+/**
+ * Affiche dans la console la somme des deux nombre entrés en paramètre
+ * @param nombre1 
+ * @param nombre2 
+ */
+function somme(nombre1:number, nombre2:number):void {
+    const resultat : number = nombre1+nombre2;
+    console.log(resultat);
+}
+
+somme(2,5);
+
+// Exercice 3
+
+
 
