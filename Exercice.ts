bcdc9334f51831412c121a9541877ef2c17a2a8b
Alexandre Bouriez <alexandre.bouriez@laposte.net>
2022-11-24 11:24:11 +0100

    feat: ajout de ma réponse à l'exercice 1

---
+++ Exercice.ts
@@ -0,0 +1,17 @@
+/* Type de variable
+* Type de retour
+* Fonction Séparées par exercice
+* uniquement fonctions natives
+* DOC ! */
+
+
+// Exercice 1
+/**
+ * Affiche un message de bienvenue dans la console
+ * @param prenom : string
+ */
+function accueil(prenom:string):void {
+    console.log(`Bonjour ${prenom} !`)
+};
+
+
